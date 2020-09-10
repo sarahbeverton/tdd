5cc118ae76efb04bfaf7347cf14bd86c34a62e25
Sarah Beverton <sebryant@gmail.com>
2020-09-10 19:16:15 -0400

    passing most tests, still need help and multi options

--- echo.py
+++ echo.py
@@ -2,22 +2,54 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility."""
 
-__author__ = "???"
+__author__ = "Sarah Beverton with some help from Joseph's help session"
 
 
 import sys
+import argparse
 
 
 def create_parser():
     """Returns an instance of argparse.ArgumentParser"""
-    # your code here
-    return
+    parser = argparse.ArgumentParser(
+            description="Perform transformation on input text.")
+    parser.add_argument('-u', '--upper', help='convert text to uppercase',
+                        action='store_true')
+    parser.add_argument('-l', '--lower', help='convert text to lowercase',
+                        action='store_true')
+    parser.add_argument('-t', '--title', help='convert text to titlecase',
+                        action='store_true')
+    parser.add_argument('text', help='text to be manipulated',
+                        type=str, nargs='+')
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    # your code here
-    return
+    parser = create_parser()
+    ns = parser.parse_args(args)
+
+    """
+    with open('USAGE') as f:
+            expected_lst = f.read().splitlines()
+            expected = '\n'.join(expected_lst)
+    print(expected)
+    """
+
+    if not ns:
+        parser.print_usage()
+        sys.exit(1)
+
+    echo_text = ' '.join(ns.text)
+
+    if ns.upper:
+        print(echo_text.upper())
+    elif ns.lower:
+        print(echo_text.lower())
+    elif ns.title:
+        print(echo_text.title())
+    else:
+        print(echo_text)
 
 
 if __name__ == '__main__':
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -7,7 +7,7 @@ Students MUST EDIT this module, to add more tests to run
 against the 'echo.py' program.
 """
 
-__author__ = "???"
+__author__ = "Sarah Beverton"
 
 import sys
 import importlib
@@ -68,9 +68,19 @@ class TestEcho(unittest.TestCase):
     # - Does it understand `--lower` ? or `--title` ?
     # - If you enable one option as true, are the rest false?
     #
+
     def test_parser_namespace(self):
-        # your code here
-        self.fail()  # replace me
+        p = self.module.create_parser()
+        args = ["-l", "HELLO WORLD"]
+        ns = p.parse_args(args)
+        self.assertFalse(ns.upper)
+        self.assertFalse(ns.title)
+        self.assertTrue(ns.lower)
+        self.assertEqual(ns.text[0], args[1])
+
+        # check if --upper and --title returns false with assert
+        # check if -l returns true with assert
+        # check if arg gives back expected result (assertequal)
 
     def test_echo(self):
         """Check if main() function prints anything at all"""
@@ -98,48 +108,66 @@ class TestEcho(unittest.TestCase):
     #
 
     def test_lower_long(self):
-        # your code here
-        self.fail()  # replace me
+        """Check if short option '--lower' performs lowercasing"""
+        args = ["--lower", "HELLO"]
+        output = run_capture(self.module.__file__, args)
+        self.assertEqual(output[0], "hello")
 
     def test_upper_short(self):
-        # your code here
-        self.fail()  # replace me
+        """Check if short option '-u' performs uppercasing"""
+        args = ["-u", "hello world"]
+        output = run_capture(self.module.__file__, args)
+        self.assertEqual(output[0], "HELLO WORLD")
 
     def test_upper_long(self):
-        # your code here
-        self.fail()  # replace me
+        """Check if long option '--upper' performs uppercasing"""
+        args = ["--upper", "hello world"]
+        output = run_capture(self.module.__file__, args)
+        self.assertEqual(output[0], "HELLO WORLD")
 
     def test_title_short(self):
-        # your code here
-        self.fail()  # replace me
+        """Check if short option '-t' performs titlecasing"""
+        args = ["-t", "hello world"]
+        output = run_capture(self.module.__file__, args)
+        self.assertEqual(output[0], "Hello World")
 
     def test_title_long(self):
-        # your code here
-        self.fail()  # replace me
+        """Check if long option '--title' performs titlecasing"""
+        args = ["--title", "hello world"]
+        output = run_capture(self.module.__file__, args)
+        self.assertEqual(output[0], "Hello World")
 
     def test_multiple_options(self):
-        # your code here
-        self.fail()  # replace me
+        """Check if multiple options '-tul' perform in correct order"""
+        args = ["-tul", "hello world"]
+        output = run_capture(self.module.__file__, args)
+        self.assertEqual(output[0], "Hello World")
 
+    ##FINISH
     def test_help_message(self):
-        # your code here
-        self.fail()  # replace me
+        """Checking for correct help message display"""
+        with open('USAGE') as f:
+            expected_lst = f.read().splitlines()
+            expected = '\n'.join(expected_lst)
+        output = run_capture(self.module.__file__, ['-h'])
+        self.assertEqual(output, expected)
 
     #
     # Students: add a flake8 test here.
     # You may borrow some test code from previous assignments!
     #
     def test_flake8(self):
-        # your code here
-        self.fail()  # replace me
-
+        """Checking for PEP8/flake8 compliance"""
+        result = subprocess.run(['flake8', self.module.__file__])
+        self.assertEqual(result.returncode, 0)
     #
     # Students: add an __author__ test here.
     # You may borrow some test code from previous assignments!
     #
+
     def test_author(self):
-        # your code here
-        self.fail()  # replace me
+        """Checking for __author__ string"""
+        self.assertNotEqual(self.module.__author__, '???')
 
 
 if __name__ == '__main__':
